9e1be0bad18d58319d0081a7b964ce7d7bb68c5e
Hasnan <monirhasnan@gmail.com>
2023-07-28 19:57:45 +0600

    slider problem fixed

--- src/Home/SwipeBanner/SwipeBanner.jsx
+++ src/Home/SwipeBanner/SwipeBanner.jsx
@@ -24,12 +24,15 @@ const SwipeBanner = () => {
       {data.map((card) => (
         <div
           key={card.id}
-          className={`panel ${activeId === card.id ? "active" : ""}`}
+          className={`panel ${
+            activeId === card.id ? "active" : ""
+          } md:h-[550px] h-[270px]`}
           onClick={() => onClick(card.id)}
           onMouseEnter={() => onClick(card.id)}
+          onMouseLeave={() => onClick(card.id === 100)}
           style={{ backgroundImage: `url(${card.url})` }}
         >
-          <h3 className="bg-black p-2 font-semibold text-[14px]">
+          <h3 className="bg-black p-2 font-semibold md:text-[18px] text-[8px] w-full">
             {card.title}
           </h3>
         </div>
