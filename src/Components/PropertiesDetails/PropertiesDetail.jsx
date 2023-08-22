e89adc99cffafd9f279b43e47eebb0cfd78d4bfc
Hasnan <monirhasnan@gmail.com>
2023-08-23 00:34:26 +0600

    details page updated

--- src/Components/PropertiesDetails/PropertiesDetail.jsx
+++ src/Components/PropertiesDetails/PropertiesDetail.jsx
@@ -14,6 +14,8 @@ import food from "../../../src/assets/details/food.jpg";
 import convention from "../../../src/assets/details/convention.jpg";
 import park from "../../../src/assets/details/park.jpg";
 import field from "../../../src/assets/details/field.jpg";
+import brochure from "../../../src/assets/brochure/brochure-left.jpg";
+import book from "../../../src/assets/brochure/book-brochure.jpg";
 
 import {
   Navigation,
@@ -32,6 +34,7 @@ import "swiper/css/scrollbar";
 import "swiper/css/effect-creative";
 import "./projectswiper.css";
 import FormReuse from "../ContactForm/FormReuse/FormReuse";
+import { data } from "autoprefixer";
 const PropertiesDetail = () => {
   const { id } = useParams();
   const [datas] = UseData();
@@ -108,57 +111,59 @@ const PropertiesDetail = () => {
       <h1 className="md:text-4xl bg-white text-center text-2xl font-semibold pt-6 md:px-[25px] md:py-[25px] py-[25px] px-[25px]">
         {dataPro[0]?.name.toUpperCase()}
       </h1>
-      <div className="bg-black lg:py-[35px] text-white grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 place-items-center lg:px-[75px] md:px-[45px] px-[25px]">
-        <div className="text-center">
-          <h1>Location</h1>
-          <p>
-            {dataPro[0]?.location?.charAt(0).toUpperCase() +
-              dataPro[0]?.location?.slice(1)}
-          </p>
-        </div>
-        <div className="text-center">
-          <h1>Plot Type</h1>
-          <p>
-            {dataPro[0]?.type1?.charAt(0).toUpperCase() +
-              dataPro[0]?.type1?.slice(1)}{" "}
-            &{" "}
-            {dataPro[0]?.type2?.charAt(0).toUpperCase() +
-              dataPro[0]?.type2?.slice(1)}
-          </p>
-        </div>
-        <div className="text-center">
-          <h1>Plot Size</h1>
-          <p>3,5 & 10 Katha</p>
-        </div>
-        <div className="text-center">
-          <h1>Completion Date</h1>
-          <p>To be announced</p>
-        </div>
-        <div className="text-center">
-          <h1>Status</h1>
-          <p>
-            {" "}
-            {dataPro[0]?.phase?.charAt(0).toUpperCase() +
-              dataPro[0]?.phase?.slice(1)}
-          </p>
+      <div className="bg-[#111518] lg:px-[75px] md:px-[45px] px-[25px]  md:py-[35px] py-[25px] lg:py-[15px]">
+        <div className=" text-white md:py-[28px] py-[15px] lg:py-[35px] grid lg:grid-cols-5 gap-y-[15px] place-items-center border-y-[1px] border-white">
+          <div className="text-center">
+            <h1>Location</h1>
+            <p>
+              {dataPro[0]?.location?.charAt(0).toUpperCase() +
+                dataPro[0]?.location?.slice(1)}
+            </p>
+          </div>
+          <div className="text-center">
+            <h1>Plot Type</h1>
+            <p>
+              {dataPro[0]?.type1?.charAt(0).toUpperCase() +
+                dataPro[0]?.type1?.slice(1)}{" "}
+              &{" "}
+              {dataPro[0]?.type2?.charAt(0).toUpperCase() +
+                dataPro[0]?.type2?.slice(1)}
+            </p>
+          </div>
+          <div className="text-center">
+            <h1>Plot Size</h1>
+            <p>3,5 & 10 Katha</p>
+          </div>
+          <div className="text-center">
+            <h1>Completion Date</h1>
+            <p>To be announced</p>
+          </div>
+          <div className="text-center">
+            <h1>Status</h1>
+            <p>
+              {" "}
+              {dataPro[0]?.phase?.charAt(0).toUpperCase() +
+                dataPro[0]?.phase?.slice(1)}
+            </p>
+          </div>
         </div>
       </div>
       {/* Overview and Spec */}
-      <div className="grid lg:grid-cols-2 grid-cols-1 bg-[#495057] lg:px-[45px] md:px-[35px] px-[25px] py-[45px] gap-[50px] text-white">
-        <div className="space-y-3">
-          <h1>Overview</h1>
+      <div className="grid lg:grid-cols-2 grid-cols-1 border-y-[3px] border-black bg-[#212529] lg:px-[45px] md:px-[35px] px-[35px] py-[45px] gap-[50px] text-white">
+        <div className="space-y-3 lg:border-r-[2px] border-white lg:border-b-[0px] border-b-[2px] pb-[35px]">
+          <h1 className="md:text-3xl text-2xl text-[#daa520]">Overview</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, odit
             sunt? Laborum quasi similique commodi nostrum aperiam eos at sequi
             inventore, fugiat consequatur sunt recusandae magni dolores amet.
             Nostrum, quo!
           </p>
-          <button className="py-[7px] px-[45px] border-indigo-500 border-[2px]">
+          <button className="px-[35px] py-[7px] text-white border-[2px] border-white hover:bg-black hover:text-white transition-all ease-in delay-100 duration-300">
             Read More
           </button>
         </div>
         <div className="space-y-3">
-          <h1>Specification</h1>
+          <h1 className="md:text-3xl text-2xl text-[#daa520]">Specification</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
             natus ut maxime facilis corrupti modi sunt illum cupiditate dolorum
@@ -169,14 +174,14 @@ const PropertiesDetail = () => {
             provident natus, a excepturi modi. Sapiente rerum cum, velit facere
             odio voluptatem atque nulla perferendis vel excepturi, accusamus.
           </p>
-          <button className="py-[7px] px-[45px] border-indigo-500 border-[2px]">
+          <button className="px-[35px] py-[7px] text-white border-[2px] border-white hover:bg-black hover:text-white transition-all ease-in delay-100 duration-300">
             Read More
           </button>
         </div>
       </div>
       {/* Feature of the city */}
-      <div className="">
-        <h1 className="md:text-3xl text-2xl text-center py-[20px] font-semibold">
+      <div className="px-[25px]">
+        <h1 className="md:text-3xl text-2xl text-center  py-[20px] font-semibold">
           Features of The City
         </h1>
         <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 gap-x-4 lg:px-[25px]">
@@ -231,11 +236,53 @@ const PropertiesDetail = () => {
         </div>
       </div>
 
+      {/* Brochure */}
+      <div className="grid lg:grid-cols-2 grid-cols-1 border-t-[3px] border-black mt-[25px]">
+        <div className="relative">
+          <img
+            className="brightness-50 w-full lg:h-full h-[450px] object-cover"
+            src={brochure}
+            alt=""
+          />
+          <div className="absolute lg:top-[25%] top-[8%] py-[35px] px-[45px] space-y-[20px] ">
+            <h1 className="text-[#daa520] md:text-3xl text-xl">Plot Booking</h1>
+            <p className="text-white text-xl">Regulation</p>
+            <p className="text-white text-xl">
+              Welcome to{" "}
+              <span className="font-semibold">{dataPro[0]?.name}</span>! We are
+              excited to have you as a potential plot buyer. To ensure a smooth
+              and transparent process, we have established the following plot
+              booking regulations.
+            </p>
+            <button className="px-[35px] py-[7px] text-white border-[2px] border-white hover:bg-black hover:text-white transition-all ease-in delay-100 duration-300">
+              Read More
+            </button>
+          </div>
+        </div>
+        <div className="relative">
+          <img
+            className="w-full lg:h-full h-[450px] object-cover brightness-50 "
+            src={book}
+            alt=""
+          />
+          <div className="absolute top-1/2 lg:left-[35%] md:left-[30%] left-[25%]">
+            <a
+              className="px-[35px] py-[10px] text-white border-[2px] border-white hover:bg-black hover:text-white transition-all ease-in delay-100 duration-300"
+              href={dataPro[0]?.brochure}
+              target="blank"
+            >
+              <button className="md:text-xl text-[16px]">
+                Download Brochure
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
       {/* Video Section */}
 
       <>
         <iframe
-          className="w-full h-[550px] pt-[30px]"
+          className="w-full h-[550px]"
           src="https://www.youtube.com/embed/y9j-BL5ocW8"
           title="Cinematic Real estate video tour example 4K | Laowa 12mm &amp; Sony A7III"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
