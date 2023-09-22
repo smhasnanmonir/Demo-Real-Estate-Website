2946e03569c08be1ecafbffba7ed0dde328e97f5
S.M Hasnan Monir <monirhasnan@gmail.com>
2023-09-22 11:36:41 +0600

    performance improved with lazy loader

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,14 +1,29 @@
+import React from "react";
+
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
-import Home from "../Home/Home/Home";
-import PropertiesDetail from "../Components/PropertiesDetails/PropertiesDetail";
-import ContactForm from "../Components/ContactForm/ContactForm";
-import PropertyReuseStructure from "../Components/PropertyCard/PropertyReuseStructure";
-import OurStory from "../Components/AboutUs/OurStory";
-import OurClient from "../Components/AboutUs/OurClient";
-import OurTeam from "../Components/AboutUs/OurTeam";
-import MissionVision from "../Components/AboutUs/MissionVission";
-import CareerPage from "../Components/CareerPage/CareerPage";
+
+const Home = React.lazy(() => import("../Home/Home/Home"));
+const PropertiesDetail = React.lazy(() =>
+  import("../Components/PropertiesDetails/PropertiesDetail")
+);
+const ContactForm = React.lazy(() =>
+  import("../Components/ContactForm/ContactForm")
+);
+const PropertyReuseStructure = React.lazy(() =>
+  import("../Components/PropertyCard/PropertyReuseStructure")
+);
+const OurStory = React.lazy(() => import("../Components/AboutUs/OurStory"));
+const OurClient = React.lazy(() => import("../Components/AboutUs/OurClient"));
+
+const OurTeam = React.lazy(() => import("../Components/AboutUs/OurTeam"));
+
+const MissionVision = React.lazy(() =>
+  import("../Components/AboutUs/MissionVission")
+);
+const CareerPage = React.lazy(() =>
+  import("../Components/CareerPage/CareerPage")
+);
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,27 +31,87 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home></Home>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <Home></Home>
+          </React.Suspense>
+        ),
       },
       {
         path: "/allproperties",
-        element: <PropertyReuseStructure></PropertyReuseStructure>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <PropertyReuseStructure></PropertyReuseStructure>
+          </React.Suspense>
+        ),
       },
       {
         path: "/allproperties/:selectedValue",
-        element: <PropertyReuseStructure></PropertyReuseStructure>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <PropertyReuseStructure></PropertyReuseStructure>
+          </React.Suspense>
+        ),
       },
       {
         path: "/properties/:id",
-        element: <PropertiesDetail></PropertiesDetail>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <PropertiesDetail></PropertiesDetail>
+          </React.Suspense>
+        ),
       },
       {
         path: "/contact",
-        element: <ContactForm></ContactForm>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <ContactForm></ContactForm>
+          </React.Suspense>
+        ),
       },
       {
         path: "/career",
-        element: <CareerPage></CareerPage>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <CareerPage></CareerPage>
+          </React.Suspense>
+        ),
       },
       {
         path: "/business",
@@ -55,14 +130,6 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "/career",
-        element: (
-          <div>
-            <h1 className="text-center min-h-screen">This is career page</h1>
-          </div>
-        ),
-      },
-      {
         path: "/*",
         element: (
           <div className="text-center py-[10%]">
@@ -77,19 +144,53 @@ const router = createBrowserRouter([
       },
       {
         path: "/aboutus/ourstory",
-        element: <OurStory></OurStory>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <OurStory />
+          </React.Suspense>
+        ),
       },
       {
         path: "/aboutus/ourclients",
-        element: <OurClient></OurClient>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <OurClient></OurClient>
+          </React.Suspense>
+        ),
       },
       {
         path: "/aboutus/ourteam",
-        element: <OurTeam></OurTeam>,
+        element: (
+          <React.Suspense
+            fallback={
+              <div className="grid place-items-center min-h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+              </div>
+            }
+          >
+            <OurTeam></OurTeam>
+          </React.Suspense>
+        ),
       },
       {
         path: "/aboutus/missionVision",
-        element: <MissionVision></MissionVision>,
+        element: (
+          <React.Suspense fallback={<div>Loading</div>}>
+            <MissionVision></MissionVision>
+          </React.Suspense>
+        ),
       },
     ],
   },
